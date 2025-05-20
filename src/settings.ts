acfbfc269f82d30a57673542d96381da01fdb21a
Mara <mara-li@outlook.fr>
2025-05-20 23:26:49 +0200

    feat: update sorting logic and improve internationalization support

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,13 @@
-import { type App, MarkdownRenderer, PluginSettingTab, Setting } from "obsidian";
+import {
+	type App,
+	MarkdownRenderer,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 import type { SortMarkdownListSettings } from "./interfaces";
 import type SortMarkdownList from "./main";
 import dedent from "dedent";
+import i18next from "i18next";
 
 export class MarkdownListSortSettings extends PluginSettingTab {
 	plugin: SortMarkdownList;
@@ -17,43 +23,63 @@ export class MarkdownListSortSettings extends PluginSettingTab {
 		const { containerEl } = this;
 		containerEl.empty();
 
-		const markdownDesc = dedent(`
-		The frontmatter allows to use automatically the right commands based on the frontmatter. Conveniant when used with the linter plugin and other macros.
+		const markdownDesc = dedent(`${i18next.t("settings.description")}
 		
-		<u>Keys</u>:
-		- \`sml_reverse\` *(\`boolean\`)* : Reverse the order of the sort (Z-A)
-		- \`sml_advanced\` *(\`boolean\`)*: Use the advanced sort (each letter will be a heading)
-		- \`sml_level\` *(\`number\`)* : Heading level for the advanced sort (1-6)
-		- \`sml_sort\` *(\`boolean\`)* : Disable the command (can be forced using other commands)
+		<u>${i18next.t("keys")}</u>:
+		- \`sml_descending\` *(\`boolean\`)* : ${i18next.t("settings.reverse")}
+		- \`sml_advanced\` *(\`boolean\`)*: ${i18next.t("settings.advanced")}
+		- \`sml_level\` *(\`number\`)* : ${i18next.t("settings.level")}
+		- \`sml_sort\` *(\`boolean\`)* : ${i18next.t("settings.disable")}
 		`);
 
-		await MarkdownRenderer.render(this.app, markdownDesc, containerEl, "", this.plugin);
+		await MarkdownRenderer.render(
+			this.app,
+			markdownDesc,
+			containerEl,
+			"",
+			this.plugin,
+		);
 
 		new Setting(containerEl)
-			.setName("Heading level")
+			.setName(`${i18next.t("level")}`)
 			.addSlider((slider) =>
-				slider.setValue(this.settings.sml_level).setLimits(1, 6, 1).setDynamicTooltip()
+				slider
+					.setValue(this.settings.sml_level)
+					.setLimits(1, 6, 1)
+					.onChange(async (value) => {
+						this.settings.sml_level = value;
+						await this.plugin.saveSettings();
+					})
+					.setDynamicTooltip(),
 			);
 
-		new Setting(containerEl).setName("Sort").addToggle((toggle) =>
-			toggle.setValue(this.settings.sml_sort).onChange(async (value) => {
-				this.settings.sml_sort = value;
-				await this.plugin.saveSettings();
-			})
-		);
+		new Setting(containerEl)
+			.setName(`${i18next.t("sort")}`)
+			.addToggle((toggle) =>
+				toggle.setValue(this.settings.sml_sort).onChange(async (value) => {
+					this.settings.sml_sort = value;
+					await this.plugin.saveSettings();
+				}),
+			);
 
-		new Setting(containerEl).setName("Reverse").addToggle((toggle) =>
-			toggle.setValue(this.settings.sml_reverse).onChange(async (value) => {
-				this.settings.sml_reverse = value;
-				await this.plugin.saveSettings();
-			})
-		);
+		new Setting(containerEl)
+			.setName(`${i18next.t("reverse")}`)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.settings.sml_descending)
+					.onChange(async (value) => {
+						this.settings.sml_descending = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 
-		new Setting(containerEl).setName("Advanced").addToggle((toggle) =>
-			toggle.setValue(this.settings.sml_advanced).onChange(async (value) => {
-				this.settings.sml_advanced = value;
-				await this.plugin.saveSettings();
-			})
-		);
+		new Setting(containerEl)
+			.setName(`${i18next.t("advanced")}`)
+			.addToggle((toggle) =>
+				toggle.setValue(this.settings.sml_advanced).onChange(async (value) => {
+					this.settings.sml_advanced = value;
+					await this.plugin.saveSettings();
+				}),
+			);
 	}
 }
